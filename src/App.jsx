b7092561391ef71fb4369506142c688cd6a7e656
Daniel mpm <131329348+dmarcosweb@users.noreply.github.com>
2023-06-23 17:21:05 -0300

    → Finished ←

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Menu from "./Menu";
+import Title from "./Title";
+import menu from "./data";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text={"Our menu"} />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
